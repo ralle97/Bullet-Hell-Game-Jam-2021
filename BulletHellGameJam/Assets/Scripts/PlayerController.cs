4a5aacc44104caac29ce72300b81a28170ad86a4
ralle97 <48724134+ralle97@users.noreply.github.com>
2021-04-15 21:14:30 +0200

    Initial commit
    Added preexisting assets

---
+++ BulletHellGameJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3f;
+
+    private float horizontal;
+    private float vertical;
+
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidBody.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        position.y += vertical * speed * Time.deltaTime;
+
+        rigidBody.MovePosition(position);
+    }
+}
