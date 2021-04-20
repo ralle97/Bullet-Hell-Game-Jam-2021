f16d79cc0c9d3855d47e7375f53f0cb58b0894f4
ralle97 <48724134+ralle97@users.noreply.github.com>
2021-04-20 19:32:35 +0200

    Boss
    Boss added;
    Animations for boss done;
    Particle effects for boss done;

---
+++ BulletHellGameJam/Assets/Scripts/BossEnemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : Enemy
+{
+    /*
+    [SerializeField]
+    private Enemy.EnemyStats stats;
+    */
+
+}
