cb6f1d3765daf448cf28c877a0e0a12126dd40ea
ralle97 <48724134+ralle97@users.noreply.github.com>
2021-05-04 22:20:11 +0200

    Update SettingsMenu.cs

--- BulletHellGameJam/Assets/Scripts/SettingsMenu.cs
+++ BulletHellGameJam/Assets/Scripts/SettingsMenu.cs
@@ -55,6 +55,6 @@ public class SettingsMenu : MonoBehaviour
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); 
     }
 }
